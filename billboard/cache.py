89517ccc37db3685fe48677c96c4990ae9b207ac
atrox3d <atrox3d@gmail.com>
2021-03-08 11:37:12 +0100

    remove cache list

--- billboard/cache.py
+++ billboard/cache.py
@@ -4,7 +4,7 @@ CACHE_DIR = "billboard-cache"
 class BillboardCache:
     def __init__(self, cache_dir=CACHE_DIR):
         self.cache_dir = cache_dir
-        self.cache = []
+        # self.cache = []
 
     def load(self, date):
         with open(f"{self.cache_dir}/{date}.html", mode="r", encoding="utf-8") as cachefile:
@@ -16,11 +16,12 @@ class BillboardCache:
             cachefile.write(page)
 
     def is_cached(self, date):
-        if date in self.cache:
-            try:
-                with open(f"{self.cache_dir}/{date}.html", mode="r", encoding="utf-8") as _:
-                    return True
-            except FileNotFoundError:
-                return False
-        else:
+        # if date in self.cache:
+        try:
+            with open(f"{self.cache_dir}/{date}.html", mode="r", encoding="utf-8") as _:
+                # self.cache.append(date)
+                return True
+        except FileNotFoundError:
             return False
+        # else:
+        #     return False
